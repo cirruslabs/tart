a06b188b5d4d799afe29fa87dd19040cdf9c13b6
Nikolay Edigaryev <edigaryev@gmail.com>
2022-05-03 16:11:50 +0300

    tart clone: pull the VM if it's OCI-based first

--- Sources/tart/Commands/Clone.swift
+++ Sources/tart/Commands/Clone.swift
@@ -13,6 +13,12 @@ struct Clone: AsyncParsableCommand {
 
   func run() async throws {
     do {
+      // Pull the VM in case it's OCI-based and doesn't exist locally yet
+      if let remoteName = try? RemoteName(sourceName), !VMStorageOCI().exists(remoteName) {
+        let registry = try Registry(host: remoteName.host, namespace: remoteName.namespace)
+        try await VMStorageOCI().pull(remoteName, registry: registry)
+      }
+
       try VMStorageHelper.open(sourceName).clone(to: VMStorageLocal().create(newName))
 
       Foundation.exit(0)
