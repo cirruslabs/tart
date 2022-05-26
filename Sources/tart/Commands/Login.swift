a650d747e1b0140941ee99c092faace9f9cc82c2
Nikolay Edigaryev <edigaryev@gmail.com>
2022-05-26 11:41:45 +0300

    Make DictionaryCredentialsProvider fileprivate to Login.swift

--- Sources/tart/Commands/Login.swift
+++ Sources/tart/Commands/Login.swift
@@ -34,3 +34,19 @@ struct Login: AsyncParsableCommand {
     }
   }
 }
+
+fileprivate class DictionaryCredentialsProvider: CredentialsProvider {
+  var credentials: Dictionary<String, (String, String)>
+
+  init(_ credentials: Dictionary<String, (String, String)>) {
+    self.credentials = credentials
+  }
+
+  func retrieve(host: String) throws -> (String, String)? {
+    credentials[host]
+  }
+
+  func store(host: String, user: String, password: String) throws {
+    credentials[host] = (user, password)
+  }
+}
