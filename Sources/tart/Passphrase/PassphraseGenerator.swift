1e90752ded5281458dfcd92c82e8af72dd1d0237
Nikolay Edigaryev <edigaryev@gmail.com>
2022-06-20 18:53:17 +0300

    Full-fledged VNC support (#126)

---
+++ Sources/tart/Passphrase/PassphraseGenerator.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+struct PassphraseGenerator: Sequence {
+    func makeIterator() -> PassphraseIterator {
+        PassphraseIterator()
+    }
+}
+
+struct PassphraseIterator: IteratorProtocol {
+    mutating func next() -> String? {
+        passphrases[Int(arc4random_uniform(UInt32(passphrases.count)))]
+    }
+}
