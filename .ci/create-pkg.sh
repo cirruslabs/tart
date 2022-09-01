ac64c01dec5dfcba14d9683819f90e3e1727ed90
fedor <fedor.korotkov@gmail.com>
2022-09-01 11:37:56 -0400

    Include version in installer

--- .ci/create-pkg.sh
+++ .ci/create-pkg.sh
@@ -2,8 +2,10 @@
 
 set -e
 
+export VERSION="${CIRRUS_TAG:-0}"
+
 mkdir -p .ci/pkg/
 cp .build/arm64-apple-macosx/debug/tart .ci/pkg/
-pkgbuild --root .ci/pkg --version "${CIRRUS_TAG:-0}" --install-location /usr/local/bin/ --identifier com.github.cirruslabs.tart --sign "Developer ID Installer: Fedor Korotkov (9M2P8L4D89)" ./dist/Tart.pkg
-xcrun notarytool submit ./dist/Tart.pkg --keychain-profile "notarytool" --wait
-xcrun stapler staple ./dist/Tart.pkg
+pkgbuild --root .ci/pkg --version $VERSION --install-location /usr/local/bin/ --identifier com.github.cirruslabs.tart --sign "Developer ID Installer: Fedor Korotkov (9M2P8L4D89)" "./dist/Tart-$VERSION.pkg"
+xcrun notarytool submit "./dist/Tart-$VERSION.pkg" --keychain-profile "notarytool" --wait
+xcrun stapler staple "./dist/Tart-$VERSION.pkg"
