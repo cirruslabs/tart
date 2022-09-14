f9001304c877faf71af38eb9477815dde0497d0c
Fedor Korotkov <fedor.korotkov@gmail.com>
2022-09-14 10:06:22 -0400

    Fixed packaging

--- .ci/create-pkg.sh
+++ .ci/create-pkg.sh
@@ -6,6 +6,6 @@ export VERSION="${CIRRUS_TAG:-0}"
 
 mkdir -p .ci/pkg/
 cp .build/arm64-apple-macosx/debug/tart .ci/pkg/
-pkgbuild --root .ci/pkg --version $VERSION --install-location /usr/local/bin/ --identifier com.github.cirruslabs.tart --sign "Developer ID Installer: Fedor Korotkov (9M2P8L4D89)" "./dist/Tart-$VERSION.pkg"
+pkgbuild --root .ci/pkg --version $VERSION --install-location /usr/local/bin/ --identifier com.github.cirruslabs.tart --sign "Developer ID Installer: Cirrus Labs, Inc. (9M2P8L4D89)" "./dist/Tart-$VERSION.pkg"
 xcrun notarytool submit "./dist/Tart-$VERSION.pkg" --keychain-profile "notarytool" --wait
 xcrun stapler staple "./dist/Tart-$VERSION.pkg"
