133a5732ac653b0d209b7435ed28bc2986850fd4
Nikolay Edigaryev <edigaryev@gmail.com>
2022-11-09 19:16:01 +0400

    Explain why we disable cookies

--- Sources/tart/Fetcher.swift
+++ Sources/tart/Fetcher.swift
@@ -4,6 +4,14 @@ import AsyncAlgorithms
 fileprivate func getURLSession() -> URLSession {
   let config = URLSessionConfiguration.default
 
+  // Harbor expects a CSRF token to be present if the HTTP client
+  // carries a session cookie between its requests[1] and fails if
+  // it was not present[2].
+  //
+  // To fix that, we disable the automatic cookies carry in URLSession.
+  //
+  // [1]: https://github.com/goharbor/harbor/blob/a4c577f9ec4f18396207a5e686433a6ba203d4ef/src/server/middleware/csrf/csrf.go#L78
+  // [2]: https://github.com/cirruslabs/tart/issues/295
   config.httpShouldSetCookies = false
 
   return URLSession(configuration: config)
