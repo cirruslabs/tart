bc793c74bed543eab964daa3d177459d39aa5531
marc-48k <61566036+marc-48k@users.noreply.github.com>
2022-12-14 11:02:33 +0000

    Adds Option to filter VMs by 'source'

--- Sources/tart/Commands/List.swift
+++ Sources/tart/Commands/List.swift
@@ -5,18 +5,30 @@ import SwiftUI
 struct List: AsyncParsableCommand {
   static var configuration = CommandConfiguration(abstract: "List created VMs")
 
-  @Flag(name: [.short, .long], help: ArgumentHelp("Only display VM names"))
+  @Flag(name: [.short, .long], help: ArgumentHelp("Only display VM names."))
   var quiet: Bool = false
+  
+  @Option(name: [.short, .long], help: ArgumentHelp("Only display VMs of this Source."))
+  var source: String = ""
 
   func run() async throws {
     do {
       if !quiet {
         print("Source\tName")
       }
-
-      displayTable("local", try VMStorageLocal().list())
-      displayTable("oci", try VMStorageOCI().list().map { (name, vmDir, _) in (name, vmDir) })
-
+      
+      switch source {
+      case "local":
+        displayTable("local", try VMStorageLocal().list())
+      case "oci":
+        displayTable("oci", try VMStorageOCI().list().map { (name, vmDir, _) in (name, vmDir) })
+      case "":
+        displayTable("local", try VMStorageLocal().list())
+        displayTable("oci", try VMStorageOCI().list().map { (name, vmDir, _) in (name, vmDir) })
+      default:
+        throw ValidationError("Unknown source: '\(source)'")
+      }
+      
       Foundation.exit(0)
     } catch {
       print(error)
