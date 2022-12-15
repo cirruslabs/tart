d30cf8c241ba49782440eb7bed9bc76afc216ed9
marc-48k <61566036+marc-48k@users.noreply.github.com>
2022-12-15 12:08:52 +0000

    Adds validation in case multiple options are supplied.

--- Sources/tart/Commands/Get.swift
+++ Sources/tart/Commands/Get.swift
@@ -4,24 +4,24 @@ import Foundation
 struct Get: AsyncParsableCommand {
   static var configuration = CommandConfiguration(commandName: "get", abstract: "Get a VM's configuration")
 
-  @Argument(help: "VM name")
+  @Argument(help: "VM name.")
   var name: String
 
-  @Flag(help: "Number of VM CPUs")
+  @Flag(help: "Number of VM CPUs.")
   var cpu: Bool = false
 
-  @Flag(help: "VM memory size in megabytes")
+  @Flag(help: "VM memory size in megabytes.")
   var memory: Bool = false
 
-  @Flag(help: "Disk size in gigabytes")
+  @Flag(help: "Disk size in gigabytes.")
   var diskSize: Bool = false
 
-  @Flag(help: "VM display resolution in a format of <width>x<height>. For example, 1200x800")
+  @Flag(help: "VM display resolution in a format of <width>x<height>. For example, 1200x800.")
   var display: Bool = false
 
   func validate() throws {
     if [cpu, memory, diskSize, display].filter({$0}).count > 1 {
-      throw ValidationError("--cpu, --memory, --disk-size and --display are mutually exclusive")
+      throw ValidationError("Options --cpu, --memory, --disk-size and --display are mutually exclusive")
     }
   }
 
