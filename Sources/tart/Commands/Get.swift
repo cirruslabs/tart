fdeaf979c26ef0097eb2be81f2fe300e66185096
marc-48k <61566036+marc-48k@users.noreply.github.com>
2022-12-15 18:52:22 +0000

    Get command (#353)
    * (wip) First pass at Get command.
    
    * Adds validation in case multiple options are supplied.

--- Sources/tart/Commands/Get.swift
+++ Sources/tart/Commands/Get.swift
@@ -4,20 +4,49 @@ import Foundation
 struct Get: AsyncParsableCommand {
   static var configuration = CommandConfiguration(commandName: "get", abstract: "Get a VM's configuration")
 
-  @Argument(help: "VM name")
+  @Argument(help: "VM name.")
   var name: String
+
+  @Flag(help: "Number of VM CPUs.")
+  var cpu: Bool = false
+
+  @Flag(help: "VM memory size in megabytes.")
+  var memory: Bool = false
+
+  @Flag(help: "Disk size in gigabytes.")
+  var diskSize: Bool = false
+
+  @Flag(help: "VM display resolution in a format of <width>x<height>. For example, 1200x800.")
+  var display: Bool = false
+
+  func validate() throws {
+    if [cpu, memory, diskSize, display].filter({$0}).count > 1 {
+      throw ValidationError("Options --cpu, --memory, --disk-size and --display are mutually exclusive")
+    }
+  }
 
   func run() async throws {
     let vmDir = try VMStorageLocal().open(name)
     let vmConfig = try VMConfig(fromURL: vmDir.configURL)
-    let diskSize = try vmDir.sizeBytes() / 1000 / 1000 / 1000
+    let diskSizeInGb = try vmDir.sizeBytes() / 1000 / 1000 / 1000
+    let memorySizeInMb = vmConfig.memorySize  / 1024 / 1024
 
-    print("CPU\tMemory\tDisk\tDisplay")
-
-    var s = "\(vmConfig.cpuCount)\t"
-    s += "\(vmConfig.memorySize / 1024 / 1024) MB\t"
-    s += "\(diskSize) GB\t"
-    s += "\(vmConfig.display.width)x\(vmConfig.display.height)"
-    print(s)
+    if cpu {
+      print(vmConfig.cpuCount)
+    } else if memory {
+      print(memorySizeInMb)
+    } else if diskSize {
+      print(diskSizeInGb)
+    } else if display {
+      print("\(vmConfig.display.width)x\(vmConfig.display.height)")
+    } else {
+      print(
+        "CPU\tMemory\tDisk\tDisplay\n" +
+          "\(vmConfig.cpuCount)\t" +
+          "\(memorySizeInMb) MB\t" +
+          "\(diskSizeInGb) GB\t" +
+          "\(vmConfig.display)"
+      )
+    }
   }
 }
