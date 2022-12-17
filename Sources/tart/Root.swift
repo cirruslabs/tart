0101a3c9f26fc7d6b231416f57d18db76c5a3335
fedor <fedor.korotkov@gmail.com>
2022-12-17 10:28:03 -0500

    lint issue

--- Sources/tart/Root.swift
+++ Sources/tart/Root.swift
@@ -99,7 +99,7 @@ struct Root: AsyncParsableCommand {
       if exitCode(for: error).rawValue == 0 {
         exit(withError: error)
       }
-      
+
       // Capture the error into Sentry
       SentrySDK.capture(error: error)
       SentrySDK.flush(timeout: 2.seconds.timeInterval)
