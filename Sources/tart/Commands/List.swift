7d513de5febff59940e3033f33f99f02b7639778
fedor <fedor.korotkov@gmail.com>
2023-02-03 10:14:20 -0500

    Print size last

--- Sources/tart/Commands/List.swift
+++ Sources/tart/Commands/List.swift
@@ -4,8 +4,8 @@ import SwiftUI
 
 fileprivate struct VMInfo: Encodable {
   let Source: String
-  let Size: Int
   let Name: String
+  let Size: Int
 }
 
 struct List: AsyncParsableCommand {
@@ -31,13 +31,13 @@ struct List: AsyncParsableCommand {
     var infos: [VMInfo] = []
     if source == nil || source == "local" {
       infos += sortedInfos(try VMStorageLocal().list().map { (name, vmDir) in
-        try VMInfo(Source: "local", Size: vmDir.sizeGB(), Name: name)
+        try VMInfo(Source: "local", Name: name, Size: vmDir.sizeGB())
       })
     }
 
     if source == nil || source == "oci" {
       infos += sortedInfos(try VMStorageOCI().list().map { (name, vmDir, _) in
-        try VMInfo(Source: "oci", Size: vmDir.sizeGB(), Name: name)
+        try VMInfo(Source: "oci", Name: name, Size: vmDir.sizeGB())
       })
     }
     print(format.renderList(infos))
