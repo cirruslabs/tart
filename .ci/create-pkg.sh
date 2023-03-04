5e44116b69d53e8470370b6cfdb9b19ebff5843d
Fedor Korotkov <fedor.korotkov@gmail.com>
2023-03-03 20:42:22 -0500

    Build Package installer and notarize release archive (#436)
    * Build Package Installer
    
    * Fixed typo
    
    * Notarize archive
    
    * Try lowercase
    
    * Use zip to satisfy notarization
    
    * Always upload dist
    
    * Don't duplicate binaries
    
    * Fancier Homebrew install block
    
    * Can't stable zip
    
    * Renamed artifacts instruction
    
    * Fixed config
    
    * upload pkg
    
    * Lowercase
    
    * don't package app-structure

--- .ci/create-pkg.sh
+++ .ci/create-pkg.sh
@@ -4,12 +4,12 @@ set -e
 
 export VERSION="${CIRRUS_TAG:-0}"
 
-mkdir -p .ci/pkg/Tart.app/Contents/MacOS
-cp .build/arm64-apple-macosx/debug/tart .ci/pkg/Tart.app/Contents/MacOS/Tart
-cp Resources/embedded.provisionprofile .ci/pkg/Tart.app/Contents/embedded.provisionprofile
-pkgbuild --root .ci/pkg/Tart.app --identifier com.github.cirruslabs.tart --version $VERSION \
+mkdir -p .ci/pkg/
+cp .build/arm64-apple-macosx/debug/tart .ci/pkg/tart
+cp Resources/embedded.provisionprofile .ci/pkg/embedded.provisionprofile
+pkgbuild --root .ci/pkg/ --identifier com.github.cirruslabs.tart --version $VERSION \
   --scripts .ci/pkg/scripts \
-  --install-location "/Library/Application Support/Tart/Tart.app" \
+  --install-location "/Library/Application Support/Tart" \
   --sign "Developer ID Installer: Cirrus Labs, Inc. (9M2P8L4D89)" \
   "./.ci/Tart-$VERSION.pkg"
 xcrun notarytool submit "./.ci/Tart-$VERSION.pkg" --keychain-profile "notarytool" --wait
