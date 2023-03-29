236f27b44fc709ba13a162d0ef85e6cd27815c1c
Nikolay Edigaryev <edigaryev@gmail.com>
2023-03-29 16:17:03 +0400

    Upgrade Sentry to 8.3.3

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     .package(url: "https://github.com/antlr/antlr4", branch: "dev"),
     .package(url: "https://github.com/apple/swift-atomics.git", .upToNextMajor(from: "1.0.0")),
     .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.50.6"),
-    .package(url: "https://github.com/getsentry/sentry-cocoa", from: "7.31.3"),
+    .package(url: "https://github.com/getsentry/sentry-cocoa", from: "8.3.3"),
     .package(url: "https://github.com/cfilipov/TextTable", branch: "master"),
   ],
   targets: [
