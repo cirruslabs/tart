8e75a59d542e1bafa2f1c1e45544acbe41f8040a
fedor <fedor.korotkov@gmail.com>
2023-04-06 10:36:07 -0400

    Revert "Revert pkg (#462)"
    This reverts commit 3fdf82079a7e7fc0bdcd64a3cb5d37c29ded8cf9.

--- .ci/create-pkg.sh
+++ .ci/create-pkg.sh
@@ -6,12 +6,8 @@ export VERSION="${CIRRUS_TAG:-0}"
 
 mkdir -p .ci/pkg/
 cp .build/arm64-apple-macosx/release/tart .ci/pkg/tart
-codesign --sign "Developer ID Application: Cirrus Labs, Inc. (9M2P8L4D89)" \
-  --options=runtime --deep \
-  --entitlements Resources/tart-prod.entitlements --force \
-  .ci/pkg/tart
 cp Resources/embedded.provisionprofile .ci/pkg/embedded.provisionprofile
-pkgbuild --root .ci/pkg/ --identifier com.github.cirruslabs.tart-app --version $VERSION \
+pkgbuild --root .ci/pkg/ --identifier com.github.cirruslabs.tart --version $VERSION \
   --scripts .ci/pkg/scripts \
   --install-location "/Library/Application Support/Tart" \
   --sign "Developer ID Installer: Cirrus Labs, Inc. (9M2P8L4D89)" \
