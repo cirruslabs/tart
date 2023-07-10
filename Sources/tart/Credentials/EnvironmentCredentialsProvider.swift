a3d7db8218f10b8c175f8fd358799e38c056a317
Nikolay Edigaryev <edigaryev@gmail.com>
2023-07-10 17:50:54 +0400

    Introduce TART_REGISTRY_HOST

--- Sources/tart/Credentials/EnvironmentCredentialsProvider.swift
+++ Sources/tart/Credentials/EnvironmentCredentialsProvider.swift
@@ -2,6 +2,11 @@ import Foundation
 
 class EnvironmentCredentialsProvider: CredentialsProvider {
   func retrieve(host: String) throws -> (String, String)? {
+    if let tartRegistryHost = ProcessInfo.processInfo.environment["TART_REGISTRY_HOST"],
+       tartRegistryHost != host {
+      return nil
+    }
+
     let username = ProcessInfo.processInfo.environment["TART_REGISTRY_USERNAME"]
     let password = ProcessInfo.processInfo.environment["TART_REGISTRY_PASSWORD"]
     if let username = username, let password = password {
