a00d7cec49bfcae884d9b25cfd5e0d716915c19e
Stanisław Chmiela <sjchmiela@users.noreply.github.com>
2023-07-11 14:01:45 +0200

    Update Sources/tart/Commands/Prune.swift
    Co-authored-by: Fedor Korotkov <fedor.korotkov@gmail.com>

--- Sources/tart/Commands/Prune.swift
+++ Sources/tart/Commands/Prune.swift
@@ -34,9 +34,7 @@ struct Prune: AsyncParsableCommand {
     // Clean up cache entries based on last accessed date
     if let olderThan = olderThan {
       let olderThanInterval = Int(exactly: olderThan)!.days.timeInterval
-      // olderThanInterval is positive (eg. 7 days) - we need to _subtract_
-      // the interval from current time to get "older than" date - hence * -1.
-      let olderThanDate = Date().addingTimeInterval(olderThanInterval * -1)
+      let olderThanDate = Date() - olderThanInterval
 
       try Prune.pruneOlderThan(olderThanDate: olderThanDate)
     }
