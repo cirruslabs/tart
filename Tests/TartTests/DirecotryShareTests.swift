cb7107b236e4e5b983369ecda1198a536e30ae86
fedor <fedor.korotkov@gmail.com>
2023-07-12 15:02:23 -0400

    Fixed tests

--- Tests/TartTests/DirecotryShareTests.swift
+++ Tests/TartTests/DirecotryShareTests.swift
@@ -5,28 +5,28 @@ final class DirectoryShareTests: XCTestCase {
   func testNamedParsing() throws {
     let share = try DirectoryShare(parseFrom: "build:/Users/admin/build")
     XCTAssertEqual(share.name, "build")
-    XCTAssertEqual(share.path, "/Users/admin/build")
+    XCTAssertEqual(share.path, URL(filePath: "/Users/admin/build"))
     XCTAssertFalse(share.readOnly)
   }
   
   func testNamedReadOnlyParsing() throws {
     let share = try DirectoryShare(parseFrom: "build:/Users/admin/build:ro")
     XCTAssertEqual(share.name, "build")
-    XCTAssertEqual(share.path, "/Users/admin/build")
+    XCTAssertEqual(share.path, URL(filePath: "/Users/admin/build"))
     XCTAssertTrue(share.readOnly)
   }
   
   func testOptionalNameParsing() throws {
     let share = try DirectoryShare(parseFrom: "/Users/admin/build")
     XCTAssertNil(share.name)
-    XCTAssertEqual(share.path, "/Users/admin/build")
+    XCTAssertEqual(share.path, URL(filePath: "/Users/admin/build"))
     XCTAssertFalse(share.readOnly)
   }
   
   func testOptionalNameReadOnlyParsing() throws {
     let share = try DirectoryShare(parseFrom: "/Users/admin/build:ro")
     XCTAssertNil(share.name)
-    XCTAssertEqual(share.path, "/Users/admin/build")
+    XCTAssertEqual(share.path, URL(filePath: "/Users/admin/build"))
     XCTAssertTrue(share.readOnly)
   }
 }
