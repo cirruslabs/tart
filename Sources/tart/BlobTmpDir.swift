a87b06d96b5a0e2445a0e529070266543bf44ab5
Tommy <79723996+Tommygithubaccount123@users.noreply.github.com>
2023-07-13 17:30:43 -0700

    Add error cases
    - Added try catches for BlobTmpDir class

--- Sources/tart/BlobTmpDir.swift
+++ Sources/tart/BlobTmpDir.swift
@@ -2,7 +2,11 @@ import Foundation
 import AsyncAlgorithms
 
 enum BlobsTmpDirError: Error {
+  case FailedToCreateBlobsTmpDir
   case FailedToCreateBlobsDir
+  case FailedToGetBlob
+  case FailedToSetBlob
+  case FailedToGetAllBlobs
 }
 
 class BlobTmpDir {
@@ -10,53 +14,74 @@ class BlobTmpDir {
   let blobsURL : URL
 
   init(baseURL: URL) throws {
-    self.baseURL = baseURL
-    self.blobsURL = baseURL.appendingPathComponent("blobs", isDirectory: true)
-    if !FileManager.default.fileExists(atPath: blobsURL.path){
-      do {
-        try FileManager.default.createDirectory(at: blobsURL, withIntermediateDirectories: false)
-        print("Initialized BlobTmpDir")
-      } catch {
-        throw BlobsTmpDirError.FailedToCreateBlobsDir
+    do {
+      self.baseURL = baseURL
+      self.blobsURL = baseURL.appendingPathComponent("blobs", isDirectory: true)
+      if !FileManager.default.fileExists(atPath: blobsURL.path){
+        do {
+          try FileManager.default.createDirectory(at: blobsURL, withIntermediateDirectories: false)
+          print("Initialized BlobTmpDir")
+        } catch {
+          throw BlobsTmpDirError.FailedToCreateBlobsDir
+        }
+      } else {
+        print("BlobTmpDir exists, extracting")
       }
-    } else {
-      print("BlobTmpDir exists, extracting")
+
+    } catch {
+      throw BlobsTmpDirError.FailedToCreateBlobsTmpDir
     }
   }
 
   func get(name: String) throws -> Data? {
-    let blobURL = blobsURL.appendingPathComponent(name)
-    if !FileManager.default.fileExists(atPath: blobURL.path){
-      return nil
+    do {
+      let blobURL = blobsURL.appendingPathComponent(name)
+      if !FileManager.default.fileExists(atPath: blobURL.path){
+        return nil
+      }
+      let blob = try FileHandle(forReadingFrom: blobURL)
+      let data = blob.readDataToEndOfFile()
+      return data
+
+    } catch {
+      throw BlobsTmpDirError.FailedToGetBlob
     }
-    let blob = try FileHandle(forReadingFrom: blobURL)
-    let data = blob.readDataToEndOfFile()
-    return data
   }
 
   func set(contents: AsyncThrowingChannel<Data, Error>, name: String) async throws {
-    let blobURL = blobsURL.appendingPathComponent(name)
-    await FileManager.default.createFile(atPath: blobURL.path, contents: try contents.asData())
+    do {
+      let blobURL = blobsURL.appendingPathComponent(name)
+      await FileManager.default.createFile(atPath: blobURL.path, contents: try contents.asData())
+
+    } catch {
+      throw BlobsTmpDirError.FailedToSetBlob
+    }
   }
 
   func getAllBlobs() throws -> [Data] {
-    var blobs: [Data] = []
-    let fileManager = FileManager.default
-    let blobFiles = try fileManager.contentsOfDirectory(at: blobsURL, includingPropertiesForKeys: nil)
+    do {
+      var blobs: [Data] = []
+      let fileManager = FileManager.default
+      let blobFiles = try fileManager.contentsOfDirectory(at: blobsURL, includingPropertiesForKeys: nil)
 
-    //Sort blobFiles by ascending order
-    let sortedBlobFiles = blobFiles.sorted { url1, url2 in
-      let name1 = url1.lastPathComponent
-      let name2 = url2.lastPathComponent
-      return name1.localizedStandardCompare(name2) == .orderedAscending
-    }
+      //Sort blobFiles by ascending order
+      let sortedBlobFiles = blobFiles.sorted { url1, url2 in
+        let name1 = url1.lastPathComponent
+        let name2 = url2.lastPathComponent
+        return name1.localizedStandardCompare(name2) == .orderedAscending
+      }
 
-    //This loop takes a ton of memory
-    //Reads each blob file and puts into an blobs array
-    for blobFile in sortedBlobFiles{
-      let blobData = try Data(contentsOf: blobFile)
-      blobs.append(blobData)
+      //This loop takes a ton of memory
+      //Reads each blob file and puts into an blobs array
+      for blobFile in sortedBlobFiles{
+        let blobData = try Data(contentsOf: blobFile)
+        blobs.append(blobData)
+      }
+      return blobs
+
+    } catch {
+      throw BlobsTmpDirError.FailedToGetAllBlobs
     }
-    return blobs
   }
+
 }
