aca768a8389ef5d1bdc3c632a472f2f8e6a7aef4
Fedor Korotkov <fedor.korotkov@gmail.com>
2023-11-10 13:44:51 -0500

    Print put errors from Docker Helpers (#654)
    * Print put errors from Docker Helpers
    
    * Update Sources/tart/Credentials/DockerConfigCredentialsProvider.swift
    
    Co-authored-by: Nikolay Edigaryev <edigaryev@gmail.com>
    
    * Check output data is not empty
    
    ---------
    
    Co-authored-by: Nikolay Edigaryev <edigaryev@gmail.com>

--- Sources/tart/Credentials/DockerConfigCredentialsProvider.swift
+++ Sources/tart/Credentials/DockerConfigCredentialsProvider.swift
@@ -41,13 +41,18 @@ class DockerConfigCredentialsProvider: CredentialsProvider {
 
     process.waitUntilExit()
 
+    let outputData = try outPipe.fileHandleForReading.readToEnd()
     if !(process.terminationReason == .exit && process.terminationStatus == 0) {
+      if let outputData = outputData {
+        print(String(decoding: outputData, as: UTF8.self))
+      }
       throw CredentialsProviderError.Failed(message: "Docker helper failed!")
     }
+    if outputData == nil || outputData?.count == 0 {
+      throw CredentialsProviderError.Failed(message: "Docker helper output is empty!")
+    }
 
-    let getOutput = try JSONDecoder().decode(
-      DockerGetOutput.self, from: outPipe.fileHandleForReading.readDataToEndOfFile()
-    )
+    let getOutput = try JSONDecoder().decode(DockerGetOutput.self, from: outputData!)
     return (getOutput.Username, getOutput.Secret)
   }
 
