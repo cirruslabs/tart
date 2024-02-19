7bf3adefed638362b561a9d8ad9917ee20bc4777
Nikolay Edigaryev <edigaryev@gmail.com>
2024-02-20 00:40:34 +0400

    Simpler DirectoryShare parsing and --dir documentation improvements (#734)
    * Simpler DirectoryShare parsing and --dir documentation improvements
    
    * Invert the argument.count logic to be similar to the old code
    
    For easier comparison.
    
    * Ensure we can parse /Users/admin/build:tag=foo-bar,ro
    
    * Document that options are comma-separated

--- Tests/TartTests/DirecotryShareTests.swift
+++ Tests/TartTests/DirecotryShareTests.swift
@@ -1,6 +1,8 @@
 import XCTest
 @testable import tart
 
+import Virtualization
+
 final class DirectoryShareTests: XCTestCase {
   func testNamedParsing() throws {
     let share = try DirectoryShare(parseFrom: "build:/Users/admin/build")
@@ -37,11 +39,36 @@ final class DirectoryShareTests: XCTestCase {
     XCTAssertFalse(share.readOnly)
     XCTAssertEqual(share.mountTag, "foo-bar")
 
-
     let roShare = try DirectoryShare(parseFrom: "/Users/admin/build:ro,tag=foo-bar")
     XCTAssertNil(roShare.name)
     XCTAssertEqual(roShare.path, URL(filePath: "/Users/admin/build"))
     XCTAssertTrue(roShare.readOnly)
     XCTAssertEqual(roShare.mountTag, "foo-bar")
+
+    let inverseRoShare = try DirectoryShare(parseFrom: "/Users/admin/build:tag=foo-bar,ro")
+    XCTAssertNil(inverseRoShare.name)
+    XCTAssertEqual(inverseRoShare.path, URL(filePath: "/Users/admin/build"))
+    XCTAssertTrue(inverseRoShare.readOnly)
+    XCTAssertEqual(inverseRoShare.mountTag, "foo-bar")
+  }
+
+  func testURL() throws {
+    let archiveWithoutNameOrOptions = try DirectoryShare(parseFrom: "https://example.com/archive.tar.gz")
+    XCTAssertNil(archiveWithoutNameOrOptions.name)
+    XCTAssertEqual(archiveWithoutNameOrOptions.path, URL(string: "https://example.com/archive.tar.gz")!)
+    XCTAssertFalse(archiveWithoutNameOrOptions.readOnly)
+    XCTAssertEqual(archiveWithoutNameOrOptions.mountTag, VZVirtioFileSystemDeviceConfiguration.macOSGuestAutomountTag)
+
+    let archiveWithOptions = try DirectoryShare(parseFrom: "https://example.com/archive.tar.gz:ro,tag=sometag")
+    XCTAssertNil(archiveWithOptions.name)
+    XCTAssertEqual(archiveWithOptions.path, URL(string: "https://example.com/archive.tar.gz")!)
+    XCTAssertTrue(archiveWithOptions.readOnly)
+    XCTAssertEqual(archiveWithOptions.mountTag, "sometag")
+
+    let archiveWithNameAndOptions = try DirectoryShare(parseFrom: "somename:https://example.com/archive.tar.gz:ro,tag=sometag")
+    XCTAssertEqual(archiveWithNameAndOptions.name, "somename")
+    XCTAssertEqual(archiveWithNameAndOptions.path, URL(string: "https://example.com/archive.tar.gz")!)
+    XCTAssertTrue(archiveWithNameAndOptions.readOnly)
+    XCTAssertEqual(archiveWithNameAndOptions.mountTag, "sometag")
   }
 }
