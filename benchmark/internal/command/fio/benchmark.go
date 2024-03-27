7d0ca6ddce1875cf84e99fad93cb0848e122163c
Nikolay Edigaryev <edigaryev@gmail.com>
2024-03-27 12:04:11 +0400

    benchmark(fio): increase runtime to 30 seconds

--- benchmark/internal/command/fio/benchmark.go
+++ benchmark/internal/command/fio/benchmark.go
@@ -8,22 +8,22 @@ type Benchmark struct {
 var benchmarks = []Benchmark{
 	{
 		Name: "Random writing of 1MB",
-		Command: "fio --rw randwrite --runtime 10 --time_based --unlink 1 --output-format json " +
+		Command: "fio --rw randwrite --runtime 30 --time_based --unlink 1 --output-format json " +
 			"--size 1MB --name unnamed",
 	},
 	{
 		Name: "Random writing of 10MB",
-		Command: "fio --rw randwrite --runtime 10 --time_based --unlink 1 --output-format json " +
+		Command: "fio --rw randwrite --runtime 30 --time_based --unlink 1 --output-format json " +
 			"--size 10MB --name unnamed",
 	},
 	{
 		Name: "Random writing of 100MB",
-		Command: "fio --rw randwrite --runtime 10 --time_based --unlink 1 --output-format json " +
+		Command: "fio --rw randwrite --runtime 30 --time_based --unlink 1 --output-format json " +
 			"--size 100MB --name unnamed",
 	},
 	{
 		Name: "Random writing of 1000MB",
-		Command: "fio --rw randwrite --runtime 10 --time_based --unlink 1 --output-format json " +
+		Command: "fio --rw randwrite --runtime 30 --time_based --unlink 1 --output-format json " +
 			"--size 1000MB --name unnamed",
 	},
 }
