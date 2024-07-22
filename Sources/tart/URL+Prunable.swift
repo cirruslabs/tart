e7659cc7a08f82d1207466f7d0350acdaca24858
fedor <fedor.korotkov@gmail.com>
2024-07-22 09:59:17 -0400

    Show how much deduplication happening
    Improvement to the APFS deduplication logic which checks whether a disk image file `mayShareFileContent` with some other file, and then we put a custom attribute to track the deduplication since there is no way to get this information from APFS itself.
    
    It's not 100% accurate but given that OCI cache is immutable the actual disk usage can only be lover than that.

--- Sources/tart/URL+Prunable.swift
+++ Sources/tart/URL+Prunable.swift
@@ -1,4 +1,5 @@
 import Foundation
+import XAttr
 
 extension URL: Prunable {
   var url: URL {
@@ -10,10 +11,30 @@ extension URL: Prunable {
   }
 
   func allocatedSizeBytes() throws -> Int {
-    try resourceValues(forKeys: [.totalFileAllocatedSizeKey]).totalFileAllocatedSize!
+    let values = try resourceValues(forKeys: [.totalFileAllocatedSizeKey, .mayShareFileContentKey])
+    // make sure the file's origin file is there and duplication works
+    var dedublicatedSize = 0
+    if values.mayShareFileContent == true {
+      dedublicatedSize = Int(deduplicatedBytes())
+    }
+    return values.totalFileAllocatedSize! - dedublicatedSize
   }
 
   func sizeBytes() throws -> Int {
     try resourceValues(forKeys: [.totalFileSizeKey]).totalFileSize!
+  }
+
+  func setDeduplicatedBytes(_ size: UInt64) {
+    var intVal = size
+    let data = Data(bytes: &intVal, count: MemoryLayout.size(ofValue: intVal))
+    try! self.setExtendedAttribute(name: "user.deduplicatedBytes", value: data)
+  }
+
+  func deduplicatedBytes() -> UInt64 {
+    let data = try? self.extendedAttributeValue(forName: "user.deduplicatedBytes")
+    if data == nil {
+      return 0
+    }
+    return data!.withUnsafeBytes { $0.load(as: UInt64.self) }
   }
 }
