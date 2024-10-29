770220f905b57f4efcca8fcac3a8cc908ad82440
Fedor Korotkov <fedor.korotkov@gmail.com>
2024-10-29 02:01:47 -0700

    Fixed plist file in version update (#927)

--- .ci/set-version.sh
+++ .ci/set-version.sh
@@ -4,4 +4,4 @@ TMPFILE=$(mktemp)
 envsubst < Sources/tart/CI/CI.swift > $TMPFILE
 mv $TMPFILE Sources/tart/CI/CI.swift
 
-/usr/libexec/PlistBuddy -c "Add :CFBundleShortVersionString string ${CIRRUS_TAG}" Info.plist
+/usr/libexec/PlistBuddy -c "Add :CFBundleShortVersionString string ${CIRRUS_TAG}" Resources/Info.plist
