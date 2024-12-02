b40cbc8058390652dc4780007ec40d7da749e802
Nikolay Edigaryev <edigaryev@gmail.com>
2024-12-02 15:09:58 +0100

    Remove random writing tests to make space for more read/read-write tests

--- benchmark/internal/command/fio/benchmark.go
+++ benchmark/internal/command/fio/benchmark.go
@@ -7,26 +7,6 @@ type Benchmark struct {
 
 var benchmarks = []Benchmark{
 	{
-		Name: "Random writing of 1MB",
-		Command: "fio --rw randwrite --runtime 30 --time_based --unlink 1 --output-format json " +
-			"--size 1MB --name unnamed --numjobs 1 --iodepth 1 --end_fsync 1",
-	},
-	{
-		Name: "Random writing of 10MB",
-		Command: "fio --rw randwrite --runtime 30 --time_based --unlink 1 --output-format json " +
-			"--size 10MB --name unnamed --numjobs 1 --iodepth 1 --end_fsync 1",
-	},
-	{
-		Name: "Random writing of 100MB",
-		Command: "fio --rw randwrite --runtime 30 --time_based --unlink 1 --output-format json " +
-			"--size 100MB --name unnamed --numjobs 1 --iodepth 1 --end_fsync 1",
-	},
-	{
-		Name: "Random writing of 1000MB",
-		Command: "fio --rw randwrite --runtime 30 --time_based --unlink 1 --output-format json " +
-			"--size 1000MB --name unnamed --numjobs 1 --iodepth 1 --end_fsync 1",
-	},
-	{
 		// Ars Technica's "Single 4KiB random write process" test[1]
 		// with JSON output and created file cleanup
 		//
