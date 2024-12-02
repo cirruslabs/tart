e38a377f0a0b4aa5f7be9bbb773123ef6d461328
Nikolay Edigaryev <edigaryev@gmail.com>
2024-12-02 15:09:32 +0100

    Reduce file I/O size from 16 to 10 GB to avoid "No space left on device"

--- benchmark/internal/command/fio/benchmark.go
+++ benchmark/internal/command/fio/benchmark.go
@@ -47,11 +47,12 @@ var benchmarks = []Benchmark{
 	},
 	{
 		// Ars Technica's "16 parallel 64KiB random write processes" test[1]
-		// with JSON output and created file cleanup
+		// with JSON output, created file cleanup and reduced file I/O size
+		// from 16 to 10 GB to avoid "No space left on device".
 		//
 		// [1]: https://arstechnica.com/gadgets/2020/02/how-fast-are-your-disks-find-out-the-open-source-way-with-fio/
 		Name: "Single 1MiB random write process",
-		Command: "fio --name=random-write --ioengine=posixaio --rw=randwrite --bs=1m --size=16g --numjobs=1 --iodepth=1 --runtime=60 --time_based --end_fsync=1" +
+		Command: "fio --name=random-write --ioengine=posixaio --rw=randwrite --bs=1m --size=10g --numjobs=1 --iodepth=1 --runtime=60 --time_based --end_fsync=1" +
 			" --output-format json --unlink 1",
 	},
 }
