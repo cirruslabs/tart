b259a76d2f9586c26e62563d0b7224ef5aa8ed14
Niklas Holloh <niklas.holloh@adesso-mobile.de>
2025-02-03 18:41:07 +0100

    docker-auth-config-env-support: DOCKER_AUTH_CONFIG tests

--- Sources/tart/Utils.swift
+++ Sources/tart/Utils.swift
@@ -22,3 +22,36 @@ func resolveBinaryPath(_ name: String) -> URL? {
 
   return nil
 }
+
+// MARK: - Protocols for system interfaces to make them mockable
+
+// MARK: ProcessInfo
+/// A protocol resembling required interfaces of `ProcessInfo`.
+protocol ProcessInformation {
+  /// The variable names (keys) and their values in the environment from which the process was launched.
+  var environment: [String : String] { get }
+}
+
+extension ProcessInfo: ProcessInformation { }
+
+// MARK: FileManager
+protocol FileManaging {
+  /// Returns a Boolean value that indicates whether a file or directory exists at a specified path.
+  func fileExists(atPath path: String) -> Bool
+  
+  /// Returns the Data contents of the file at the given `url`. Uses `Data(contentsOf:options:)`.
+  func data(contentsOf url: URL, options: Data.ReadingOptions) throws -> Data
+}
+
+extension FileManaging {
+  /// Returns the Data contents of the file at the given `url`. Uses `Data(contentsOf:options:)`.
+  func data(contentsOf url: URL) throws -> Data {
+    return try data(contentsOf: url, options: [])
+  }
+}
+
+extension FileManager: FileManaging {
+  func data(contentsOf url: URL, options: Data.ReadingOptions) throws -> Data {
+    return try Data(contentsOf: url, options: options)
+  }
+}
---
+++ Tests/TartTests/Util/MockError.swift
@@ -0,0 +1,12 @@
+//
+//  MockError.swift
+//  Tart
+//
+//  Created by Holloh, Niklas on 03.02.25.
+//
+
+import Foundation
+
+enum MockError: Error {
+  case mockNotConfigured
+}
