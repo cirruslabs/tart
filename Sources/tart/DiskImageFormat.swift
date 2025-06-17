dc17c78b7cc73596f8af96aa55c42876792bc5fa
fedor <fedor.korotkov@gmail.com>
2025-06-17 11:18:40 -0400

    Remove canCreate property and simplify DiskImageFormat
    - Remove canCreate property since it's the same as isSupported
    - Remove description property entirely as it's not used
    - Fix displayName for RAW format (remove UDIF reference)
    - Remove checkDiskutilASIFSupport helper function
    
    Addresses review comments:
    - https://github.com/cirruslabs/tart/pull/1094#discussion_r2152109450
    - https://github.com/cirruslabs/tart/pull/1094#discussion_r2152115610
    - https://github.com/cirruslabs/tart/pull/1094#discussion_r2152124330

--- Sources/tart/DiskImageFormat.swift
+++ Sources/tart/DiskImageFormat.swift
@@ -14,14 +14,6 @@ enum DiskImageFormat: String, CaseIterable, Codable {
     }
   }
 
-  var description: String {
-    switch self {
-    case .raw:
-      return "Traditional disk image format, compatible with all macOS versions"
-    case .asif:
-      return "High-performance sparse disk image format, requires macOS 26 or newer"
-    }
-  }
 
   /// Check if the format is supported on the current system
   var isSupported: Bool {
@@ -37,45 +29,7 @@ enum DiskImageFormat: String, CaseIterable, Codable {
     }
   }
 
-  /// Check if the format can be created on the current system
-  var canCreate: Bool {
-    switch self {
-    case .raw:
-      return true
-    case .asif:
-      // ASIF creation requires diskutil with ASIF support (macOS 26+)
-      return checkDiskutilASIFSupport()
-    }
-  }
 
-  /// Check if diskutil supports ASIF format creation
-  private func checkDiskutilASIFSupport() -> Bool {
-    guard let diskutilURL = resolveBinaryPath("diskutil") else {
-      return false
-    }
-
-    let process = Process()
-    process.executableURL = diskutilURL
-    process.arguments = ["image", "create", "blank", "--help"]
-
-    let pipe = Pipe()
-    process.standardOutput = pipe
-    process.standardError = pipe
-
-    do {
-      try process.run()
-      process.waitUntilExit()
-
-      let data = pipe.fileHandleForReading.readDataToEndOfFile()
-      let output = String(data: data, encoding: .utf8) ?? ""
-
-      // Check if ASIF format is mentioned in the help output
-      // Look for ASIF in the format options list
-      return output.contains("ASIF") && output.contains("--format")
-    } catch {
-      return false
-    }
-  }
 }
 
 extension DiskImageFormat: ExpressibleByArgument {
